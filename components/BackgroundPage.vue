a187a6ae1a9687d148ce27adc3f6fe058f665dca
seppeclottemans <seppe.clottemans@student.ehb.be>
2021-02-27 16:33:25 +0100

    feat(components): add page background component

---
+++ components/BackgroundPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <img class="w-full background" :src="require(`~/assets/img/${img}.jpg`)" :alt="alt">
+</template>
+
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      default: "page background"
+    }
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.background{
+  height: 100%;
+  position: fixed;
+  background-repeat: no-repeat;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  object-fit: cover;
+}
+
+</style>
--- pages/components.vue
+++ pages/components.vue
@@ -3,7 +3,12 @@
     <h1 class="text-center mb-4">Component Library</h1>
     <ChallengeCard img="cake" title="Bake a cake" caption="Search a recipe for a cake and try to make it.
 Once you have completed the challenge you can upload a photo here."/>
+
     <Button title="Login" color="#1BA711"/>
+
+    <BackgroundPage img="night-sky" alt="image of tree in the night sky."/>
+
+    
   </div>
 </template>
 
