e8607ac1fb334b09083eae3bbd36ca6f2a10962c
seppeclottemans <seppe.clottemans@student.ehb.be>
2021-03-07 15:42:53 +0100

    feat(nav): add top navigation component

---
+++ components/TopNav.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-full h-20 bg-white flex flex-wrap content-center grid grid-cols-3 place-content-center bottom-border">
+    <font-awesome-icon @click="$router.back()" class="self-center ml-5 text-2xl back-arrow" icon="arrow-left"/>
+    <h2 class="page-title justify-self-center">{{ pageTitle }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pageTitle: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.page-title{
+  font-size: 16pt;
+}
+
+.back-arrow{
+  color: #089082;
+}
+
+.bottom-border{
+  border-bottom: 1px solid #707070;
+}
+
+</style>
